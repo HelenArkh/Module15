34bee889cafd219f50f0258e080d39ce740902c7
HelenArkh <alena.arkh@gmail.com>
2021-03-20 17:07:27 +0300

    Агрегация

--- Module15/Module15/Program.cs
+++ Module15/Module15/Program.cs
@@ -6,32 +6,42 @@ namespace Module15
 {
     class Program
     {
+        //   статическая переменная для хранения данных в памяти
+        public static List<int> Numbers = new List<int>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите текст:");
-
-            // читаем ввод
-            var text = Console.ReadLine();
-
-            // сохраняем список знаков препинания и символ пробела в коллекцию
-            var punctuation = new List<char>() { ' ', ',', '.', ';', ':', '!', '?' };
-
-            // валидация ввода
-            if (string.IsNullOrEmpty(text))
+            while (true)
             {
-                Console.WriteLine("Вы ввели пустой текст");
-                return;
+                // Читаем введенный с консоли  текст
+                var input = Console.ReadLine();
+
+                // проверяем, число ли это
+                var isInteger = Int32.TryParse(input, out int inputNum);
+
+                // если не число - показываем ошибку
+                if (!isInteger)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Вы ввели не число");
+                }
+                // если соответствует, запускаем обработчик
+                else
+                {
+                    // добавляем в список
+                    Numbers.Add(inputNum);
+
+                    // выводим все критерии
+                    Console.WriteLine("Число " + input + " добавлено в список.");
+                    Console.WriteLine($"Всего в списке  { Numbers.Count} чисел");
+                    Console.WriteLine($"Сумма:  {Numbers.Sum()}");
+                    Console.WriteLine($"Наибольшее:  {Numbers.Max()}");
+                    Console.WriteLine($"Наименьшее:  {Numbers.Min()}");
+                    Console.WriteLine($"Среднее:  {Numbers.Average()}");
+
+                    Console.WriteLine();
+                }
             }
-
-            Console.WriteLine();
-            Console.WriteLine("Текст без знаков препинания: ");
-
-            // так как строка - это массив char, мы можем вызвать метод  except  и удалить знаки препинания
-            var noPunctuation = text.Except(punctuation).ToArray();
-
-            // вывод
-            Console.WriteLine(noPunctuation);
-        }
+            }
 
         static int CountCommon(string word1, string word2)
         {
